07f59f9cb6426e56930a5beb9279bc6076dc764e
Lucana <k.ark.scarlet72@gmail.com>
2023-04-02 01:15:50 +0900

    [Finish]退会確認画面の作成、public/users#withdrawalの定義、退会処理の確認。

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,6 @@
 class Public::UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id])  # idでユーザーを取得
   end
 
   def edit
@@ -9,14 +9,19 @@ class Public::UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
+    @user.update(user_params)  # user_paramsを使用してユーザー情報をアップデート
     redirect_to user_path(@user)
   end
 
   def unsubscribe
+    @user = current_user
   end
 
   def withdrawal
+    @user = current_user
+    @user.update(is_deleted: true)  # is_deletedをtrueに切り替え
+    reset_session  # ログインセッションを終了
+    redirect_to root_path
   end
 
 
@@ -24,6 +29,6 @@ class Public::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname)
+    params.require(:user).permit(:nickname)  # :nicknameの変更を受け付ける
   end
 end
