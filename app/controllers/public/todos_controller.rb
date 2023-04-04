04d04401c338fe67335a6ceb2fb683f4b991f669
Lucana <k.ark.scarlet72@gmail.com>
2023-04-04 22:37:11 +0900

    [Finish]public/todos#newの定義と新規作成画面を重要度を除いてひとまず作成

--- app/controllers/public/todos_controller.rb
+++ app/controllers/public/todos_controller.rb
@@ -1,5 +1,6 @@
 class Public::TodosController < ApplicationController
   def new
+    @todo = Todo.new
   end
 
   def create
@@ -19,4 +20,12 @@ class Public::TodosController < ApplicationController
 
   def destroy
   end
+
+
+  private
+  
+  def todo_params
+    params.require(:todo).permit(:title, :body, :is_done, :importance)
+  end
+
 end
