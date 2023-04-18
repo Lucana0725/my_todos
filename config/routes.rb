3bff98543d5f51fd5992638f62ca0dbad9d95c21
Lucana <k.ark.scarlet72@gmail.com>
2023-04-18 23:30:30 +0900

    [Finish]admin/usersコントローラー、show, edit, updateページの生成、homes/topページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,35 @@
 Rails.application.routes.draw do
+  # ルートページ
   root to: 'public/homes#top'
 
+  # admin用Devise用ルーティング
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
 
+  # public用Devise用ルーティング
   devise_for :users, skip: [:passwords], controllers: {
     registrations: 'public/registrations',
     sessions: 'public/sessions'
   }
 
+  # admin用ログイン後遷移ページ
   get '/admin' => 'admin/homes#top', as: 'admin_top'
+
+
+  # admin用ルーティング
+  scope module: :admin do
+    resources :users, only: [:show, :edit, :update]
+  end
   
+  # public用ルーティング
   scope module: :public do
     get 'users/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
     patch 'users/withdrawal' => 'users#withdrawal', as: 'withdrawal'
 
     # patch 'todos/:id/check' => 'todos#check', as: 'check'  # public/todos/show.html.erbのチェックボックス更新用に作ったけどダメだった
-
+    
+    # public用各種resources
     resources :users, only: [:show, :edit, :update]
     resources :todos
   end
